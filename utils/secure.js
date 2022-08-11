1dfc895bae0355bd901ce0e42935ca921f59a6db
Sito8943 <sito8943@gmail.com>
2022-08-10 20:21:45 -0700

    headers for cors

--- utils/secure.js
+++ utils/secure.js
@@ -6,7 +6,15 @@ const verifyBearer = (auth) => {
   return false;
 };
 
+const headers = {
+  Accept: "application/json",
+  "Content-Type": "application/json",
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "*",
+};
+
 module.exports = {
   verifyBearer,
   keys,
+  headers,
 };
